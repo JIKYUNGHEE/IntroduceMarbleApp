e6f1503057a3a61ef22e58c512ba69e4b2c7e243
JIKYUNGHEE <wlrudgml517@gmail.com>
2022-10-17 08:38:28 +0900

    bug 원인 파악. 개선점 찾는 중. //FIXME. [BUG] 아이템 4개만 반복해서 나옴 > 원인: 해당 콜백이 4번만 불린다. 그 다음 스크롤 시 나오는 아이템들은 해당 콜백이 호출되지 않고, 기존에 있던 정보를 재활용한다. //TODO. 스크롤 할 때 마다, 해당 함수가 호출되게 한다. && 문제요구사항인 스크롤할 때마다 20개의 데이터를 호출하는 것도 추가해야 됨 !

--- app/src/main/java/kr/co/ganeg/introducemarvelapp/adapter/CharacterAdapter.kt
+++ app/src/main/java/kr/co/ganeg/introducemarvelapp/adapter/CharacterAdapter.kt
@@ -15,6 +15,10 @@ class CharacterAdapter() :
 
     private var mItems: MutableList<CharacterModel> = mutableListOf()
 
+    companion object {
+        private const val TAG = "CharacterAdapter"
+    }
+
     init {
         setHasStableIds(true)
     }
@@ -29,15 +33,19 @@ class CharacterAdapter() :
         )
     }
 
+    //FIXME. [BUG] 아이템 4개만 반복해서 나옴 > 원인: 해당 콜백이 4번만 불린다. 그 다음 스크롤 시 나오는 아이템들은 해당 콜백이 호출되지 않고, 기존에 있던 정보를 재활용한다.
+    //TODO. 스크롤 할 때 마다, 해당 함수가 호출되게 한다. && 문제요구사항인 스크롤할 때마다 20개의 데이터를 호출하는 것도 추가해야 됨 !
     override fun onBindViewHolder(holder: CharacterViewHolder, position: Int) {
+        Log.e(TAG, "onBindViewHolder($position) called!!!!!!!!!!")
+        Log.d(TAG, "mItems: $mItems")    //mItems 에 값이 잘 들어옴
         val character = getItem(position)
+        Log.i(TAG, "character: $character")
         if (character != null) {
             holder.bind(character)
         }
-
     }
 
-    fun getItem(position: Int): CharacterModel? {
+    private fun getItem(position: Int): CharacterModel? {
         if (mItems == null) {
             return null
         }
@@ -46,10 +54,10 @@ class CharacterAdapter() :
 
     fun setItems(results: ArrayList<CharacterModel>) {
         if (results == null) {
-            Log.e("CharacterAdapter", "setItems result is null")
+            Log.e(TAG, "setItems result is null")
             return
         }
-        Log.d("CharacterAdapter", "response.data.results to collection: $results")
+        Log.d(TAG, "response.data.results to collection: $results")
 
         mItems.clear()
         mItems.addAll(results)
@@ -68,7 +76,7 @@ class CharacterAdapter() :
         fun bind(item: CharacterModel?) {
             binding.apply {
                 character = item
-                Log.e("KHJI", "character: $character")
+                Log.e(TAG, "character: $character")
                 Glide.with(binding.root.context)
                     .load(character?.thumbnail)
                     .into(binding.characterItemImage)
