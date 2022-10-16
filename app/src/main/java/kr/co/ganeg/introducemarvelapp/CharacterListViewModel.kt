46d1d14c96fd958bd56bf59c9873c387f2ec9061
JIKYUNGHEE <wlrudgml517@gmail.com>
2022-10-17 07:03:54 +0900

    Style: 코드정리(CharacterListViewModel.kt)

--- app/src/main/java/kr/co/ganeg/introducemarvelapp/CharacterListViewModel.kt
+++ app/src/main/java/kr/co/ganeg/introducemarvelapp/CharacterListViewModel.kt
@@ -4,19 +4,14 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import kr.co.ganeg.introducemarvelapp.api.MarvelResult
-import kr.co.ganeg.introducemarvelapp.data.CharacterDataWrapper
 import kr.co.ganeg.introducemarvelapp.model.CharacterModel
 import kr.co.ganeg.introducemarvelapp.repository.CharacterListRepository
 
 class CharacterListViewModel(application: Application): AndroidViewModel(application) {
 
-    private var characterListRepository: CharacterListRepository
-    private var characterResponseLiveData: LiveData<MarvelResult<CharacterModel>>
-
-    init {
-        characterListRepository = CharacterListRepository()
-        characterResponseLiveData = characterListRepository.getCharacterListLiveData()
-    }
+    private var characterListRepository: CharacterListRepository = CharacterListRepository()
+    private var characterResponseLiveData: LiveData<MarvelResult<CharacterModel>> =
+        characterListRepository.getCharacterListLiveData()
 
     fun getCharacterList(query: String?, page: Int) {
         characterListRepository.getCharacterList(query, page, 20)
